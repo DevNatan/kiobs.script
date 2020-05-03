53d37057507673b6c748bff65b97650cde42f866
DevNatan <itroken99@gmail.com>
2020-05-03 18:07:58 -0300

    Do not use Empty as all scripts would share the same instance

--- src/main/kotlin/me/devnatan/kiobs/script/resolver/ScriptResolver.kt
+++ src/main/kotlin/me/devnatan/kiobs/script/resolver/ScriptResolver.kt
@@ -1,3 +1,5 @@
+@file:JvmMultifileClass
+@file:JvmName("ScriptResolver")
 package me.devnatan.kiobs.script.resolver
 
 import me.devnatan.kiobs.script.ScriptInfo
@@ -5,20 +7,17 @@ import me.devnatan.kiobs.script.annotations.Script
 import me.devnatan.kiobs.script.scriptInfo
 import kotlin.script.experimental.api.*
 
-internal fun resolveScriptInfo(ctx: ScriptConfigurationRefinementContext): ResultWithDiagnostics<ScriptCompilationConfiguration> {
+fun resolveScriptInfo(ctx: ScriptConfigurationRefinementContext): ResultWithDiagnostics<ScriptCompilationConfiguration> {
     val annotations = ctx.collectedData?.get(ScriptCollectedData.foundAnnotations)
         ?.takeIf { it.isNotEmpty() } ?: return ctx.compilationConfiguration.asSuccess()
 
     return ScriptCompilationConfiguration(ctx.compilationConfiguration) {
-        val info = ScriptInfo.Empty()
         for (annotation in annotations) {
             when (annotation) {
                 is Script -> {
-                    info.name = annotation.name
+                    scriptInfo(ScriptInfo(annotation.name))
                 }
             }
         }
-
-        scriptInfo(info)
     }.asSuccess()
 }
