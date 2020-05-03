d7aa2f292bd26d1897d4d570cfbfc61d71ea3958
DevNatan <itroken99@gmail.com>
2020-05-03 17:54:15 -0300

    Add detailed diagnostics to any errors involving scripts

--- src/main/kotlin/me/devnatan/kiobs/script/ScriptException.kt
+++ src/main/kotlin/me/devnatan/kiobs/script/ScriptException.kt
@@ -5,21 +5,23 @@ import kotlin.script.experimental.api.ScriptDiagnostic
 /**
  * Thrown when there is an error in a script.
  */
-open class ScriptException : Exception()
+open class ScriptException(
+    val diagnostics: List<ScriptDiagnostic> = emptyList()
+) : Exception()
 
 /**
  * Thrown when there is an error before, during or after compiling a script.
  */
 open class ScriptCompilationError(
-    val diagnostics: List<ScriptDiagnostic>
-) : ScriptException()
+    diagnostics: List<ScriptDiagnostic>
+) : ScriptException(diagnostics)
 
 /**
  * Thrown when any source tries to evaluate a script and an error occurs.
  */
 open class ScriptEvaluationError(
-    val diagnostics: List<ScriptDiagnostic>
-) : ScriptException()
+    diagnostics: List<ScriptDiagnostic>
+) : ScriptException(diagnostics)
 
 /**
  * Launched when trying to evaluate a script that has not yet been compiled.
